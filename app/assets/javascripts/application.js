6edf495e6fdf5b2a52cff93db0221e1b649ea824
Dana <doneil1580@gmail.com>
2018-11-16 14:25:51 -0500

    navigation partial is complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+(function() {
+    var burger = document.querySelector('burger');
+    var nav = document.querySelector('#+burger.dataset.target');
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        nav.classList.toggle('is-active');
+    });
+})();
+
